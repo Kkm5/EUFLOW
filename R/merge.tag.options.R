3a3861ab4724cc19a9bc0af925f88fe4aa39d4a3
Roger Day <notsofeo@gmail.com>
2016-08-31 14:32:40 -0400

    Deleting unneeded files.

--- R/merge.tag.options.R
+++
@@ -1,7 +0,0 @@
-
-
-RNASEQDATA<-read.csv(file="RNASEQDATA.csv",header=TRUE)
-RPPADATA<-read.csv(file="RPPADATA.original.csv",header=TRUE)
-EvaluationExperimentSet<-RNASEQDATA
-ReferenceSet<-RPPADATA
-Workflow.Data<-WorkflowEvaluationData(EvaluationExperimentSet,ReferenceSet)
